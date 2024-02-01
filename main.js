e2be49230aac3e3952ad5d04215d1e770c4cabd2
9ruem2 <9ruem2@naver.com>
2024-02-01 16:43:35 +0900

    firtJs

---
+++ main.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+ 
+});
+app.listen(3000);
