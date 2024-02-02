8829d4eca52d9c0f4184d03cfac5ceb22648024c
9ruem2 <9ruem2@naver.com>
2024-02-02 14:44:13 +0900

    file을 이용한 동적 본문구현

--- main.js
+++ main.js
@@ -1,30 +1,42 @@
 var http = require('http');
 var fs = require('fs');
-var urlModule = require('url'); // 변수 이름 변경
 
 var app = http.createServer(function(request,response){
-  var _url = request.url;
-  var queryData = new URL('http://localhost:3000' + _url).searchParams;
-  console.log(queryData.get('id'));
-
-  if(_url == '/'){
-    _url = '/index.html'; // 변수 이름 변경
-  }
-  if(_url == '/favicon.ico'){
-    response.writeHead(404);
-    response.end();
-    return;
-  }
-
-  response.writeHead(200);
-  // index.html 파일 읽기 및 응답
-  fs.readFile(__dirname + _url, 'utf-8', function(err, data) {
-    if (err) {
-      response.writeHead(404);
-      response.end("Not Found");
-    } else {
-      response.end(data);
+    var _url = request.url;
+    // URL 객체를 사용하여 쿼리 스트링 추출
+    var queryData = new URL(_url, 'http://localhost:3000').searchParams;
+    var title = queryData.get('id');
+    if(_url == '/'){
+      title = 'Welcome';
     }
-  });
+    if(_url == '/favicon.ico'){
+      response.writeHead(404);
+      response.end();
+      return;
+    }
+    response.writeHead(200);
+
+    fs.readFile(`data/${queryData.get('id')}`, 'utf8', function(err,description){
+      var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.end(template);
+    });
 });
-app.listen(3000);ㅋ
+app.listen(3000);
