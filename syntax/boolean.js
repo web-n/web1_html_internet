e1187c806dd71bd25d5a7009af7edc4b2e5b1a45
9ruem2 <9ruem2@naver.com>
2024-02-02 15:31:33 +0900

    제어문

--- syntax/boolean.js
+++ syntax/boolean.js
@@ -1,4 +1,6 @@
 console.log(true);
 console.log(false);
 a = 1;
+b = 0;
 
+console.log('test');
--- syntax/program2.js
+++ syntax/program2.js
@@ -1,5 +1,5 @@
 console.log('A');
 console.log('B');
-console.log('C1');
+console.log('C2');
 console.log('D');
 
