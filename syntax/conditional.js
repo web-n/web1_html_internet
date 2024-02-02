67949e332b8e9cee9ee3db3ad0259b41663fd647
9ruem2 <9ruem2@naver.com>
2024-02-02 15:48:21 +0900

    conditional에서 input

--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,12 +1,10 @@
+var args = process.argv;
+console.log(args[2]);
 console.log('A');
 console.log('B');
-
-if(false){
-    console.log('C1');
+if(args[2] === '1'){
+  console.log('C1');
 } else {
-    console.log('C2');
+  console.log('C2');
 }
-
-
 console.log('D');
-
