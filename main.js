4bee360584a0d0d1589e8aa3a5bc788f3c8c94c8
9ruem2 <9ruem2@naver.com>
2024-02-05 12:46:58 +0900

    function2

--- main.js
+++ main.js
@@ -1,30 +1,10 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query; // queryData 변수에 저장된 것은 URL의 쿼리 스트링 부분을 키-값 쌍으로 가지는 객체
-    var pathname = url.parse(_url, true).pathname;
 
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-
-        fs.readdir('./data', function(err, filelist){
-            console.log(filelist);
-
-            var title = 'Welcome';
-            var description = 'Hello, Node.js';
-
-            var list = '<ul>';
-                var i = 0;
-                while(i < filelist.length){
-                list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                i = i + 1;
-                }
-            list = list+'</ul>';
-            
-            var template = `
-            <!doctype html>
+function templateHTML(title, list, body){
+    return `
+        <!doctype html>
             <html>
             <head>
                 <title>WEB1 - ${title}</title>
@@ -34,47 +14,46 @@ var app = http.createServer(function(request,response){
             <body>
                 <h1><a href="/">WEB</a></h1>
                 ${list}
-                <h2>${title}</h2>
-                <p>${description}</p>
+                ${body}
             </body>
             </html>
-            `;
+    `;
+}
 
+function templateList(filelist){
+    var list = '<ul>';
+    var i = 0;
+    while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+    }
+    list = list+'</ul>';
+    return list;
+}
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query; // queryData 변수에 저장된 것은 URL의 쿼리 스트링 부분을 키-값 쌍으로 가지는 객체
+    var pathname = url.parse(_url, true).pathname;
+
+    
+
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        fs.readdir('./data', function(err, filelist){
+            var title = 'Welcome';
+            var description = 'Hello, Node.js';
+            var list = templateList(filelist);
+            var temmplate = templateHTML(title,list,`<h2>${title}</h2>${descriptionbody}`);
             response.writeHead(200);
             response.end(template);
     
-        })
-      } else {
+        })} else {
             fs.readdir('./data', function(error, filelist){
-                var title = 'Welcome';
-                var description = 'Hello, Node.js';
-                var list = '<ul>';
-                var i = 0;
-                    while(i < filelist.length){
-                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                    i = i + 1;
-                    }
-                list = list+'</ul>';
-
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
                 var title = queryData.id;
-                var template = `
-
-                <!doctype html>
-                <html>
-                <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                </body>
-                </html>
-                `;
-
+                var list = templateList(filelist);
+                var temmplate = templateHTML(title,list,`<h2>${title}</h2>${descriptionbody}`);
               response.writeHead(200);
               response.end(template);
             });
