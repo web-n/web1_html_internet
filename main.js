bc339222cf07328ce91b3fe67ed2ba7fe637033c
9ruem2 <9ruem2@naver.com>
2024-02-05 15:26:19 +0900

    수정링크생성

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
     return `
         <!doctype html>
             <html>
@@ -14,7 +15,7 @@ function templateHTML(title, list, body){
             <body>
                 <h1><a href="/">WEB</a></h1>
                 ${list}
-                <a href="/create">create</a>
+                ${control}
                 ${body}
             </body>
             </html>
@@ -42,7 +43,10 @@ var app = http.createServer(function(request,response){
             var title = 'Welcome';
             var description = 'Hello, Node.js';
             var list = templateList(filelist);
-            var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
+            var template = templateHTML(title,list,
+                `<h2>${title}</h2>${description}`,
+                `<a href="/create">create</a>`
+            );
             response.writeHead(200);
             response.end(template);
     
@@ -51,7 +55,11 @@ var app = http.createServer(function(request,response){
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
                 var title = queryData.id;
                 var list = templateList(filelist);
-                var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
+                var template = templateHTML(title,list,
+                    `<h2>${title}</h2>${description}`,
+                    `<a href = "/create">create</a> <a href = "/update?id=${title}">update</a>`
+                    
+                );
               response.writeHead(200);
               response.end(template);
             });
@@ -63,7 +71,7 @@ var app = http.createServer(function(request,response){
             
             var list = templateList(filelist);
             var template = templateHTML(title,list,`
-                <form action="http://localhost:3000/process create" method = "post">
+                <form action="http://localhost:3000/create_process" method = "post">
                 <p><input type="text" name="title" placeholder ="title"></p>
                 <p>
                     <textarea name = "description" placeholder = "description"></textarea>
@@ -72,12 +80,26 @@ var app = http.createServer(function(request,response){
                     <input type = "submit">
                 </p>
                 </form>
-            `);
+            `, '');
             response.writeHead(200);
             response.end(template);
     
         });
 
+    } else if(pathname === '/create_process') {
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            fs.writeFile(`data/${title}`, description,'utf-8', function(err){
+                response.writeHead(302, {Location: `/?id=${title}`});
+                response.end('success');
+            })
+        });
     } else {
         response.writeHead(404);
         response.end('Not found');
